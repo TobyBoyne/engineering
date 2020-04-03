e9c71ef947a31d8137215b3413b5d054e60dcd9a
Toby Boyne <tobyboyne@gmail.com>
2020-04-03 12:42:25 +0100

    Computing E2Q4 Improved formatting; added second derivative function; added graph saving

--- part-ia/computing-E2Q4.py
+++ part-ia/computing-E2Q4.py
@@ -1,8 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# TODO: look at error for x=10000
-
 def f(x):
 	return x ** 2 * np.sin(x ** 2)
 
@@ -12,31 +10,37 @@ def df(x):
 def complex_step(x, h):
 	return f(x + 1j * h).imag / h
 
-if __name__ == "__main__":
+def symmetric(x, h):
+	return (f(x + h) - f(x - h)) / (2 * h)
+
+def run(approx_func):
 	xs = 10 ** np.arange(1, 5)
 	hs = 10.0 ** np.arange(-9, -16, -3)
 	rel_error = np.zeros((4, 3))
 
 	fig, ax = plt.subplots()
+	ax.set_title(f"Relative error of {approx_func.__name__} compared to exact derivative")
+	ax.set_ylabel("Relative error")
 	ax.tick_params(
 		axis='x',
-		labelbottom=False
+		labelbottom=False,
+		which='both',
+		bottom=False
 	)
-	plt.subplots_adjust(left=0.2, bottom=0.3)
+	plt.subplots_adjust(bottom=0.3)
 
 	for i, x in enumerate(xs):
-		r = abs((complex_step(x, hs) - df(x)) / df(x))
+		r = abs((approx_func(x, hs) - df(x)) / df(x))
 		rel_error[i, :] = r
 		ax.loglog(hs[::-1], r)
 
-	ax.legend(xs)
-	print(complex_step(10000, 1e-15))
-	print(df(xs))
-	for x in xs:
-		print(x, df(x))
-	# print(complex_step(10000, 1e-9) - df(10000))
-
+	ax.legend([f"x={x}" for x in xs])
 
 	plt.table(rel_error, loc='bottom',
 			  rowLabels=xs, colLabels=hs)
+	plt.savefig(f"images/comp-E2Q4-{approx_func.__name__}")
 	plt.show()
+
+if __name__ == "__main__":
+	run(approx_func=complex_step)
+	run(approx_func=symmetric)
