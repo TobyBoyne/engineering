18e9ad1421bca8e69c0923e71ee22dd6f316a243
Toby Boyne <tobyboyne@gmail.com>
2020-04-16 20:28:33 +0100

    Rewrote using standard form notation

--- part-ia/computing-E2Q1.py
+++ part-ia/computing-E2Q1.py
@@ -1,12 +1,12 @@
 import numpy as np
-n = 26 * 10 ** 6
+n = 26e6
 
 def linear(m):
-	return m * n * 10 ** -8
+	return m * n * 1e-8
 
 def binary(m):
-	return 2 * 10 ** -5 * n * np.log(n) + m * 10 ** -6 * np.log(n)
+	return 2e-5 * n * np.log(n) + m * 1e-6 * np.log(n)
 
 if __name__ == '__main__':
-	for m in (10 ** 3, 10 ** 4, 3 * 10 ** 5, 5 * 10 ** 5):
+	for m in (1e3, 1e4, 3e5, 5e5):
 		print(f'For m = {m}: linear = {linear(m)}, binary = {binary(m)}')
