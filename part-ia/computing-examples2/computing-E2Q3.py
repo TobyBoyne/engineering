1b2955b316dd62c623277ad4dfb832782934b69d
Toby Boyne <tobyboyne@gmail.com>
2020-04-17 09:31:30 +0100

    Better comparison of functions

--- part-ia/computing-examples2/computing-E2Q3.py
+++ part-ia/computing-examples2/computing-E2Q3.py
@@ -13,17 +13,25 @@ def one_sided(x, h):
 def symmetric(x, h):
 	return (f(x + h) - f(x - h)) / (2 * h)
 
+
 if __name__ == "__main__":
-	hs = 10.0 ** np.arange(-5, -10, -1)
-	x = 100
-	fig, (ax1, ax2) = plt.subplots(1, 2)
+	hs = 10.0 ** np.arange(-1, -5, -1)
+	x = 2
+	fig, ax = plt.subplots()
 	exact = df(x)
-	for func, ax in ((one_sided, ax1), (symmetric, ax2)):
-		line = np.abs(np.array([func(x, h) for h in hs]) - exact)
-		ax.loglog(hs, line, label=func.__name__)
-		# show decreasing h to the right
-		ax.invert_xaxis()
-		ax.legend()
-		ax.set_xlabel("h")
+
+	lines = {
+		"one sided": np.abs(np.array([one_sided(x, h) for h in hs]) - exact),
+		"one sided squared": np.abs(np.array([one_sided(x, h) for h in hs]) - exact) ** 2,
+		"symmetric": np.abs(np.array([symmetric(x, h) for h in hs]) - exact)
+	}
+
+	for name, line in lines.items():
+		ax.loglog(hs, line, label=name)
+
+	# show decreasing h to the right
+	ax.invert_xaxis()
+	ax.legend()
+	ax.set_xlabel("h")
 
 	plt.show()
