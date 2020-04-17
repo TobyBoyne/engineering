145ce60af83a4935047c9cc193b3048b8bed283d
Toby Boyne <tobyboyne@gmail.com>
2020-04-17 09:35:23 +0100

    Made get_line function to remove repetition

--- part-ia/computing-examples2/computing-E2Q3.py
+++ part-ia/computing-examples2/computing-E2Q3.py
@@ -14,20 +14,19 @@ def symmetric(x, h):
 	return (f(x + h) - f(x - h)) / (2 * h)
 
 
+def get_line(func):
+	return np.abs(np.array([func(x, h) for h in hs]) - exact)
+
+
 if __name__ == "__main__":
 	hs = 10.0 ** np.arange(-1, -5, -1)
 	x = 2
 	fig, ax = plt.subplots()
 	exact = df(x)
 
-	lines = {
-		"one sided": np.abs(np.array([one_sided(x, h) for h in hs]) - exact),
-		"one sided squared": np.abs(np.array([one_sided(x, h) for h in hs]) - exact) ** 2,
-		"symmetric": np.abs(np.array([symmetric(x, h) for h in hs]) - exact)
-	}
-
-	for name, line in lines.items():
-		ax.loglog(hs, line, label=name)
+	ax.loglog(hs, get_line(one_sided), label="One sided")
+	ax.loglog(hs, get_line(one_sided) ** 2, label="One sided squared", linestyle="--")
+	ax.loglog(hs, get_line(symmetric), label="Symmetric")
 
 	# show decreasing h to the right
 	ax.invert_xaxis()
