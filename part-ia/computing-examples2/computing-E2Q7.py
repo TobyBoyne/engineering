2edab28b79d7a6966dca2ba28ae35e69f69d728d
Toby Boyne <tobyboyne@gmail.com>
2020-04-17 10:47:33 +0100

    Rewrote to allow for arbitrary function by estimating derivatives

--- part-ia/computing-examples2/computing-E2Q7.py
+++ part-ia/computing-examples2/computing-E2Q7.py
@@ -1,31 +1,39 @@
 import numpy as np
 
+h = 1e-7
 
-# funcs stores each function and its derivatives
-FUNCS = {
-	"quadratic": (lambda x: x ** 2 + 3 * x + 4,
-				  lambda x: 2 * x + 3,
-				  lambda x: 2),
-	"cubic":	(lambda x: x ** 3 + x ** 2,
-				 lambda x: 3 * x ** 2 + 2 * x,
-				 lambda x: 6 * x + 2)
-}
+def df(f, x):
+	return (f(x + h) - f(x - h)) / (2 * h)
 
-def newton(f, df, ddf, x0):
+def ddf(f, x):
+	return (f(x + h) - 2 * f(x) + f(x - h)) / h ** 2
+
+
+def quadratic(x):
+	return x ** 2 + 4 * x + 3
+
+def cubic(x):
+	return x ** 3 + x ** 2
+
+
+
+def newton(f, x_0):
 	"""Return next iteration of Newton's method"""
-	return x0 - df(x0) / ddf(x0)
+	return x_0 - df(f, x_0) / ddf(f, x_0)
 
-def solve_single_var(f, df, ddf, x0=1, tol=1e-4):
-	while abs(df(x0) / ddf(x0)) > tol:
-		x0 = newton(f, df, ddf, x0)
-	return x0
+def solve_single_var(f, x_0, tol=1e-4):
+	while abs(df(f, x_0)) / abs(ddf(f, x_0)) > tol:
+		x_0 = newton(f, x_0)
+	return x_0
+
 
 if __name__ == "__main__":
+
 	# Solve 1 turning point in quadratic
-	root = solve_single_var(*FUNCS["quadratic"])
-	print(f"Cost of quadratic is minimised at x={root}")
+	root = solve_single_var(quadratic, x_0=-1)
+	print(f"Cost of quadratic is minimised at x={root:.3f}")
 
 	# Solve two turning points for cubic
-	root1 = solve_single_var(*FUNCS["cubic"], x0=1)
-	root2 = solve_single_var(*FUNCS["cubic"], x0=-5)
+	root1 = solve_single_var(cubic, x_0=1)
+	root2 = solve_single_var(cubic, x_0=-5)
 	print(f"Cost of cubic is minimised at x={root1:.4f} and x={root2:.4f}")
